c09b3fe00997a88da6ae2544968fda8be19613a1
David <88470336+MrKingDev@users.noreply.github.com>
2023-04-03 11:35:16 -0400

    Contacts Page

---
+++ data/ShopData/ContactListData.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        id: 1,
+        img: require('../../assets/images/clothes/McKee_Tee.png'),
+        position: 'Principal',
+        name: 'Gregory Bowen',
+        email: 'gbowen@schools.nyc.gov'
+    },
+    {
+        id: 2,
+        img: require('../../assets/images/clothes/McKee_Tee.png'),
+        position: 'Assistant Principal Administration/CTE/Art',
+        name: 'Agron Velija',
+        email: 'avelija@schools.nyc.gov'
+    },
+    {
+        id: 3,
+        img: require('../../assets/images/clothes/McKee_Tee.png'),
+        position: 'Assistant Principal Student Life',
+        name: 'Joanna Falkenhainer',
+        email: 'jfalkenhainer@schools.nyc.gov'
+    },
+    {
+        id: 4,
+        img: require('../../assets/images/clothes/McKee_Tee.png'),
+        position: 'Assistant Principal ELA/Social Studies/Foreign Language',
+        name: 'Brigid Howley',
+        email: 'bhowley@schools.nyc.gov'
+    },
+    {
+        id: 5,
+        img: require('../../assets/images/clothes/McKee_Tee.png'),
+        position: 'Assistant Principal Science/Mathematics/ PE ',
+        name: 'Jesse Lambert',
+        email: 'jlambert3@schools.nyc.gov'
+    },
+    {
+        id: 6,
+        img: require('../../assets/images/clothes/McKee_Tee.png'),
+        position: 'Assistant Principal Guidance and Special Education I.A.',
+        name: 'Arlene Yiadom-Daley',
+        email: 'ayiadom@schools.nyc.gov'
+    },
+]
--- screens/MainScreens/ContactScreen.js
+++ screens/MainScreens/ContactScreen.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ContactList from '../../components/Contact/ContactList';
 
 export default function ContactScreen() {
     return (
     <View style={styles.container}>
         <Text style={{fontSize:25, fontWeight:'bold'}}>Socials</Text>
+        <ContactList/>
     </View>
     );
 }
